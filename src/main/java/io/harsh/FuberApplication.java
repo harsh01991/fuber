9679dd434fffd7dddff804ee624835866afbfa69
harsh khandelwal <harshkhandelwal1991@gmail.com>
2019-03-22 08:17:08 +0530

    created new project FUBER

---
+++ src/main/java/io/harsh/FuberApplication.java
@@ -0,0 +1,13 @@
+package io.harsh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuberApplication.class, args);
+	}
+
+}
